86adc0f64e8f69311325ce01f67bfa2feb4cba4f
Malik Saad Naeem <156428364+maliksaadnaeem937@users.noreply.github.com>
2026-01-06 17:55:04 +0500

    base url removed

--- chat-app/contexts/ChatContext.jsx
+++ chat-app/contexts/ChatContext.jsx
@@ -7,9 +7,7 @@ import client from "../lib/appwrite";
 
 export const ChatContext = createContext(null);
 
-
-const BASE_URL = "http://192.168.43.79:8000";
-
+const BASE_URL = "";
 
 // 🔹 Same chatId for both users
 export const getChatId = (senderId, receiverId) =>
